b445c485b7649b71bb1f42830d2125c094835124
Victor Iscava <victor_eduardo29@hotmail.com>
2024-11-25 21:15:15 -0300

    Implementando a lógica para receber os dados da votação e criar um json

--- scripts.js
+++ scripts.js
@@ -4,60 +4,8 @@
 
 // Lista de perguntas e respostas
 const cardsData = [
-    {
-        pergunta: "MELHOR FESTA DO ANO",
-        respostas: ["Sanguinária e o Enigma do Mé", "Funkjama", "Interpoc", "Blackout", "Primeira do ano", "Operação LavaJato", "Embrulha meu Peru 2023", "Computaria"]
-    },
-    {
-        pergunta: "MELHOR CHOPPADA DO ANO",
-        respostas: ["Vira Devasta", "Nutriodonto", "Chevet", "Choppalooza", "Furacão 2000", "Funkcina", "Choppada Mafiosa", "Swing", "Sedenta", "Baile da Mamata"]
-    },
-    {
-        pergunta: "MELHOR FESTA TEMÁTICA DO ANO",
-        respostas: ["Analoween", "Mamaela", "Caloucom", "Carna Anal", "Chapados e pelados", "Operação Natal"]
-    },
-    {
-        pergunta: "MELHOR SUNSET DO ANO",
-        respostas: ["Fenda do Bikini", "Tardezinha Magnosa", "Terapinga", "Bixarada", "De volta para o passado", "SociéPagode", "Sunshark", "Melatude", "Sunset dos Exquecidos", "TaTudoRosa"]
-    },
-    {
-        pergunta: "MELHOR EVENTO ESPORTIVO DO ANO",
-        respostas: ["Adidas Sunset Party", "Bearcup", "Intercap", "Tiffu", "Mario And Luigi's World"]
-    },
-    {
-        pergunta: "MELHOR DCE DO ANO",
-        respostas: ["Prodose", "RelaMec", "Chuppadinha Sanguinária", "Baile du Nelsu", "Noite do Baguga", "DCE dos Campeões", "Melhor Sexta", "Tagafiosa"]
-    },
-    {
-        pergunta: "MELHOR ETÍLICOS DO ANO",
-        respostas: ["Colônia de Férias", "Menor Inter", "MecNutri", "Alcoolimpíadas"]
-    },
-    {
-        pergunta: "MELHOR ATRAÇÃO DO ANO",
-        respostas: ["MC Saci (Sanguinária e o Enigma do Mé)", "Bonde das Maravilhas (Blackout)", "Rick + Jacaré (Funkjama)", "Henrique de Ferraz (Primeira do Ano)", "Rennan da Penha (Vira Devasta)", "MC GW + Ramemes (Embrulha meu Peru 2023)"]
-    },
-    {
-        pergunta: "MELHOR PALCO DO ANO",
-        respostas: ["Primeira do Ano", "Sanguinária e o Enigma do Mé", "Funkjama", "Computaria"]
-    },
-    {
-        pergunta: "MELHOR QUASE CHOPPADA DO ANO",
-        respostas: ["Salva Semestre", "Operação LavaJato (1° data)", "SunSalim", "Eclipse DJS", "Civil Minha Pic*", "FarmaFunk"]
-    },
-    {
-        pergunta: "MELHOR BATERIA DO ANO",
-        respostas: ["Venenosa", "Mafiosa", "Dramática", "Sanguinária", "Analfabeta", "Tagarela", "Canina", "Madrasta"]
-    },
-    {
-        pergunta: "MELHOR DJ DO ANO",
-        respostas: ["Niko", "Yong", "Berrix", "Walker", "Lousa", "Rodrigona", "Any Volazi", "Blenu", "Georgya", "Majuu", "Samuray"]
-    },
-    {
-        pergunta: "MELHOR COLEÇÃO DO ANO",
-        respostas: ["Magnata", "Enigma", "Gambiarra", "Chefia", "Pintada", "Vale", "Overdose", "Mafiosa"]
-    },
     { // CATEGORIA ESPECIAL
-        pergunta: "ESPECIAL: ATLÉTICA COM OS PIORES BEIJOS DO ANO 🤢",
+        pergunta: "ATLÉTICA COM OS PIORES BEIJOS DO ANO 🤢",
         respostas: [
             "Tagarela", "Sedentária", "Soberana", "Malcriada", "Magnata", "Kraken", "Predadora",
             "Devasta", "Banguela", "Unidos do Vale", "Sanguinária", "Malagueta", "Sulfurosa",
@@ -66,7 +14,73 @@ const cardsData = [
             "Problemática", "Neurótica", "Vira-Lata", "Agro", "Picareta", "Subversiva",
             "Analfabeta", "Improdutiva", "Simbiótica", "Berranteira", "Mecânica", "Hamiltoniana",
             "Venenosa", "Tectônica", "Arregaçada", "Dolorosa", "Dramática"
-        ]
+        ],
+        escolha: ""
+    },
+    {
+        pergunta: "MELHOR FESTA DO ANO",
+        respostas: ["Sanguinária e o Enigma do Mé", "Funkjama", "Interpoc", "Blackout", "Primeira do ano", "Operação LavaJato", "Embrulha meu Peru 2023", "Computaria"],
+        escolha: ""
+    },
+    {
+        pergunta: "MELHOR CHOPPADA DO ANO",
+        respostas: ["Vira Devasta", "Nutriodonto", "Chevet", "Choppalooza", "Furacão 2000", "Funkcina", "Choppada Mafiosa", "Swing", "Sedenta", "Baile da Mamata"],
+        escolha: ""
+    },
+    {
+        pergunta: "MELHOR FESTA TEMÁTICA DO ANO",
+        respostas: ["Analoween", "Mamaela", "Caloucom", "Carna Anal", "Chapados e pelados", "Operação Natal"],
+        escolha: ""
+    },
+    {
+        pergunta: "MELHOR SUNSET DO ANO",
+        respostas: ["Fenda do Bikini", "Tardezinha Magnosa", "Terapinga", "Bixarada", "De volta para o passado", "SociéPagode", "Sunshark", "Melatude", "Sunset dos Exquecidos", "TaTudoRosa"],
+        escolha: ""
+    },
+    {
+        pergunta: "MELHOR EVENTO ESPORTIVO DO ANO",
+        respostas: ["Adidas Sunset Party", "Bearcup", "Intercap", "Tiffu", "Mario And Luigi's World"],
+        escolha: ""
+    },
+    {
+        pergunta: "MELHOR DCE DO ANO",
+        respostas: ["Prodose", "RelaMec", "Chuppadinha Sanguinária", "Baile du Nelsu", "Noite do Baguga", "DCE dos Campeões", "Melhor Sexta", "Tagafiosa"],
+        escolha: ""
+    },
+    {
+        pergunta: "MELHOR ETÍLICOS DO ANO",
+        respostas: ["Colônia de Férias", "Menor Inter", "MecNutri", "Alcoolimpíadas"],
+        escolha: ""
+    },
+    {
+        pergunta: "MELHOR ATRAÇÃO DO ANO",
+        respostas: ["MC Saci (Sanguinária e o Enigma do Mé)", "Bonde das Maravilhas (Blackout)", "Rick + Jacaré (Funkjama)", "Henrique de Ferraz (Primeira do Ano)", "Rennan da Penha (Vira Devasta)", "MC GW + Ramemes (Embrulha meu Peru 2023)"],
+        escolha: ""
+    },
+    {
+        pergunta: "MELHOR PALCO DO ANO",
+        respostas: ["Primeira do Ano", "Sanguinária e o Enigma do Mé", "Funkjama", "Computaria"],
+        escolha: ""
+    },
+    {
+        pergunta: "MELHOR QUASE CHOPPADA DO ANO",
+        respostas: ["Salva Semestre", "Operação LavaJato (1° data)", "SunSalim", "Eclipse DJS", "Civil Minha Pic*", "FarmaFunk"],
+        escolha: ""
+    },
+    {
+        pergunta: "MELHOR BATERIA DO ANO",
+        respostas: ["Venenosa", "Mafiosa", "Dramática", "Sanguinária", "Analfabeta", "Tagarela", "Canina", "Madrasta"],
+        escolha: ""
+    },
+    {
+        pergunta: "MELHOR DJ DO ANO",
+        respostas: ["Niko", "Yong", "Berrix", "Walker", "Lousa", "Rodrigona", "Any Volazi", "Blenu", "Georgya", "Majuu", "Samuray"],
+        escolha: ""
+    },
+    {
+        pergunta: "MELHOR COLEÇÃO DO ANO",
+        respostas: ["Magnata", "Enigma", "Gambiarra", "Chefia", "Pintada", "Unidos do Vale", "Overdose", "Mafiosa"],
+        escolha: ""
     },
 ];
 
@@ -82,12 +96,16 @@ function criarCards(data) {
 
         // Adiciona o título da pergunta
         const titulo = document.createElement("h3");
-        titulo.textContent = item.pergunta;
+        if (index == 0) {
+            titulo.innerHTML = `<span class="primeira-palavra">${"ESPECIAL: "}</span>` + item.pergunta;
+        } else {
+            titulo.textContent = item.pergunta;
+        }
         card.appendChild(titulo);
 
         // Criação do formulário com as opções
         const form = document.createElement("form");
-        if (index < data.length - 1) {
+        if (index > 0) {
             // Adiciona as opções de resposta como rádio
             item.respostas.forEach((resposta) => {
                 const label = document.createElement("label");
@@ -96,6 +114,12 @@ function criarCards(data) {
                 radio.type = "radio";
                 radio.name = `pergunta${index}`; // Agrupa as opções por pergunta
                 radio.value = resposta;
+
+                // Evento onchange para capturar a escolha
+                radio.addEventListener("change", () => {
+                    item.escolha = resposta; // Atualiza o campo "escolha"
+                    console.log(`Escolha na pergunta "${item.pergunta}": ${item.escolha}`);
+                });
 
                 label.appendChild(radio);
                 label.appendChild(document.createTextNode(` ${resposta}`));
@@ -124,6 +148,12 @@ function criarCards(data) {
                 select.appendChild(option);
             });
 
+            // Evento onchange para capturar a escolha
+            select.addEventListener("change", () => {
+                item.escolha = select.value; // Atualiza o campo "escolha"
+                console.log(`Escolha na pergunta "${item.pergunta}": ${item.escolha}`);
+            });
+
             form.appendChild(select);
         }
 
@@ -132,6 +162,42 @@ function criarCards(data) {
         gridContainer.appendChild(card);
     });
 }
+
+// Função para registrar a votação
+async function registrarVotacao() {
+    try {
+        // Obter o IP do usuário
+        const response = await fetch('https://api64.ipify.org?format=json');
+        const data = await response.json();
+        const ip = data.ip;
+
+        // Obter o datetime atual
+        const datetime = new Date().toISOString();
+
+        // Criar o objeto de votação
+        const votacao = cardsData.map(item => ({
+            pergunta: item.pergunta,
+            escolha: item.escolha || ""
+        }));
+
+        // Criar o JSON final
+        const resultado = {
+            data: datetime,
+            ip: ip,
+            votacao: JSON.stringify(votacao)
+        };
+        const resultadoString = JSON.stringify(resultado, null, 2);
+        console.log(resultadoString);
+
+        // Lógica para guardar os dados
+
+        alert("Votação registrada com sucesso!");
+    } catch (error) {
+        console.error("Erro ao registrar a votação:", error);
+        alert("Erro ao registrar a votação. Tente novamente.");
+    }
+}
+
 
 // Chama a função para criar os cards
 criarCards(cardsData);
